aa0c87a18d2eab1a33e95561665b1982597741f5
GitHub Action <action@github.com>
2021-05-16 01:09:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/mikalsvendsen/fc/MyBundle.kt
+++ src/main/kotlin/com/github/mikalsvendsen/fc/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mikalsvendsen.fc
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mikalsvendsen/fc/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mikalsvendsen/fc/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mikalsvendsen.fc.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mikalsvendsen.fc.MyBundle
 
 class MyApplicationService {
 
